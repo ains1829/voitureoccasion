542433f081b7405fbf7a3b0f693b30cf4b818a57
tafita37 <118960434+tafita37@users.noreply.github.com>
2024-01-28 13:09:36 +0100

    transmission

--- src/main/java/com/dev/controller/TransmissionController.java
+++ src/main/java/com/dev/controller/TransmissionController.java
@@ -1,7 +1,9 @@
 package com.dev.controller;
 
+import java.util.Hashtable;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,38 +19,83 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/transmission")
+@CrossOrigin("*")
 public class TransmissionController {
     private final TransmissionService transmissionService;
 
     @GetMapping(path = "/allTransmission", produces = "application/json")
-    public List<Transmission> getAllTransmission() {
-        return transmissionService.findAllTransmission();
+    public Hashtable <String,Object> getAllTransmission() {
+        Hashtable <String,Object> response=new Hashtable<>();
+        try {
+            response.put("data", transmissionService.findAllTransmission());
+            response.put("status",200);
+            response.put("message","ok");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.put("status",500);
+            response.put("message",e.getMessage());
+        }
+        return response;
     }
 
     @PostMapping("/insertTransmission")
-    public Transmission insertTransmission(@RequestBody Transmission transmission) {
-        return transmissionService.save(transmission);
+    public Hashtable <String,Object> insertTransmission(@RequestBody Transmission transmission) {
+        Hashtable <String,Object> response=new Hashtable<>();
+        try {
+            transmissionService.save(transmission);
+            response.put("status",200);
+            response.put("message","success");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.put("status",500);
+            response.put("message",e.getMessage());
+        }
+        return response;
     }
 
     @PostMapping("/updateTransmission")
-    public Transmission updateTransmission(@RequestParam int idTransmission, @RequestParam String nomTransmission) {
-        System.out.println(idTransmission+" "+nomTransmission);
-        return transmissionService.update(idTransmission, nomTransmission);
+    public Hashtable <String,Object> updateTransmission(@RequestParam int idTransmission, @RequestParam String nomTransmission) {
+        Hashtable <String,Object> response=new Hashtable<>();
+        try {
+            transmissionService.update(idTransmission, nomTransmission);
+            response.put("status",200);
+            response.put("message","success");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.put("status",500);
+            response.put("message",e.getMessage());
+        }
+        return response;
     }  
 
     @GetMapping("/findTransmissionById")
-    public Transmission findTransmissionById(@RequestParam int idTransmission) {
+    public Hashtable <String,Object> findTransmissionById(@RequestParam int idTransmission) {
+        Hashtable <String,Object> response=new Hashtable<>();
         try {
-            return transmissionService.findById(idTransmission).get();
+            response.put("data", transmissionService.findById(idTransmission).get());
+            response.put("status",200);
+            response.put("message","ok");
         } catch (Exception e) {
             e.printStackTrace();
+            response.put("status",500);
+            response.put("message",e.getMessage());
         }
-        return null;
+        return response;
     }
 
     @GetMapping("/deleteTransmission")
-    public void deleteTransmission(@RequestParam int idTransmission) {
-        transmissionService.delete(idTransmission);
+    public Hashtable <String,Object> deleteTransmission(@RequestParam int idTransmission) {
+        Hashtable <String,Object> response=new Hashtable<>();
+        try {
+            transmissionService.delete(idTransmission);
+            response.put("status",200);
+            response.put("message","success");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.put("status",500);
+            response.put("message",e.getMessage());
+        }
+        return response;
     }
 
 
